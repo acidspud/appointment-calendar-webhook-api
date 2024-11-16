9ee1d407f9b0e00426462afc15a3f097de9c69a4
Acidspud <acidspud@123.com>
2024-11-16 13:26:54 +0200

    include express webserver

---
+++ index.js
@@ -0,0 +1,24 @@
+
+
+
+const express = require('express')
+const app = express()
+
+const calendar =  require("./calendar")
+
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
+app.get('/event', async (req, res) => {
+
+    const eventStart = new Date()
+    const eventEnd = new Date(eventStart)
+    eventEnd.setHours(eventStart.getHours() + 1)
+
+    const result = await calendar.createCalendarEvent(eventStart, eventEnd, "DialoogflowAppointment")
+    // res.render("")
+    res.send(result)
+})
+
+app.listen(3000)
